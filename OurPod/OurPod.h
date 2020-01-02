4ada9e5a19c60853b50a9f04de5da8ec5d2b1510
YousefArafa <yousefahmedarafa@outlook.com>
2020-01-02 11:32:04 +0200

    first commit

---
+++ OurPod/OurPod.h
@@ -0,0 +1,19 @@
+//
+//  OurPod.h
+//  OurPod
+//
+//  Created by Yousef Arafa on 1/2/20.
+//  Copyright © 2020 Yousef Arafa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OurPod.
+FOUNDATION_EXPORT double OurPodVersionNumber;
+
+//! Project version string for OurPod.
+FOUNDATION_EXPORT const unsigned char OurPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OurPod/PublicHeader.h>
+
+
---
+++ OurPod/UIColor.swift
@@ -0,0 +1,29 @@
+//
+//  UIColor.swift
+//  JoePod
+//
+//  Created by Yousef Arafa on 12/31/19.
+//
+
+import Foundation
+import UIKit
+
+public extension UIColor {
+    
+    static var viridianGreen: UIColor {
+        return UIColor(red: 105/255, green: 132/255, blue: 116/255, alpha: 1)
+    }
+    static var amulet: UIColor {
+        return UIColor(red: 136/255, green: 158/255, blue: 129/255, alpha: 1)
+    }
+    static var rainee: UIColor {
+        return UIColor(red: 186/255, green: 199/255, blue: 167/255, alpha: 1)
+    }
+    static var whiteRock: UIColor {
+        return UIColor(red: 229/255, green: 228/255, blue: 204/255, alpha: 1)
+    }
+    static var cherub: UIColor {
+        return UIColor(red: 251/255, green: 223/255, blue: 240/255, alpha: 1)
+    }  
+}
+
